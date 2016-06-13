13ad9c7ac342c8fcdc1f57dded67bc6d4596bd1b
chaselampert <chaselampert@gmail.com>
2016-06-13 12:40:25 -0400

    blueprint to read in cvs file

--- src/com/practice/stats/Test.java
+++ src/com/practice/stats/Test.java
@@ -1,8 +1,34 @@
 package com.practice.stats;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		// -define .csv file in app
+		String fileNameDefined = "uploadedcsv/employees.csv";
+		// -File class needed to turn stringName to actual file
+		File file = new File(fileNameDefined);
+
+		try {
+			// -read from filePooped with Scanner class
+			Scanner inputStream = new Scanner(file);
+			// hashNext() loops line-by-line
+			while (inputStream.hasNext()) {
+				// read single line, put in string
+				String data = inputStream.next();
+				System.out.println(data + "***");
+
+			}
+			// after loop, close scanner
+			inputStream.close();
+
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		}
+
 	}
 }
